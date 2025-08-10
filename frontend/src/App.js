bf95c622adcaf29db8b27e5b77a7cb9282971de8
Waruna Bopitiya <161157284+waruna-bopitiya@users.noreply.github.com>
2025-08-10 18:20:00 +0530

    Landing page 30%

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <h1 className='h1'>hello</h1>
+      <center><button onClick={() => navigate("/home")}>click  </button></center>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import VehicleServiceLanding from './components/VehicleServiceLanding';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<VehicleServiceLanding />} />
+    </Routes>
+  </BrowserRouter> 
 );
 
 // If you want to start measuring performance in your app, pass a function
