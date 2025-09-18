e19781ec727594030b299720e6551d2ad51a073b
Waruna Bopitiya <161157284+waruna-bopitiya@users.noreply.github.com>
2025-09-18 17:25:04 +0530

    not wrk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import VehiclePage from "./pages/VehiclePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
-import BookingPage from "./pages/BookingPage";
+import BookingPage from "./components/Booking/BookingPage";
 
 export default function App() {
   return (
